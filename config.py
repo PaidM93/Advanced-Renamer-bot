c05f8d93134b7a28cbe7737f7883e953b79a5664
PaidM93 <143317765+PaidM93@users.noreply.github.com>
2023-09-05 10:14:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,5 +26,5 @@ class Config(object):
     CHUNK_SIZE = 128
 
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Bot99:Bot99@cluster0.pb9ryns.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL", "")
 
