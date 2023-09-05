4150497f7120fa8523cba8af9a833cfb2fec0fc3
PaidM93 <143317765+PaidM93@users.noreply.github.com>
2023-09-05 10:19:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5946994770:AAE1IzBm6Um-9BlU3fKuNBaebMTiIZldFvk")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", ))
